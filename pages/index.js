950cbf5f1508e9111bc38a908637f6d802c32ff2
Revenity <87530663+aquapi@users.noreply.github.com>
2022-05-23 13:26:59 +0700

    Adding first page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,61 @@
+// @ts-check
+import Head from "next/head";
 import React from "react";
+import 'semantic-ui-css/semantic.min.css';
+import { Header, Button } from "semantic-ui-react";
+
+/**
+ * @type {React.CSSProperties}
+ */
+const buttonStyle = {
+    width: "300px",
+    height: "80px",
+    fontSize: "20px",
+    margin: "10px",
+};
+
+/**
+ * @type {React.CSSProperties}
+ */
+const headerStyle = {
+    textAlign: 'center',
+    margin: '40px',
+}
+
+/**
+ * @type {React.CSSProperties}
+ */
+const wrapperStyle = { 
+    display: "flex", 
+    flexDirection: "column", 
+    alignItems: "center",
+    justifyContent: "center",
+};
 
 // @ts-check
 export default function IndexPage() {
+    // Check whether the user is logged in
+    const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+
     React.useEffect(() => {
-        location.replace("/home");
+        if (localStorage.getItem("token"))
+            setIsLoggedIn(true);
     });
-    return <></>;
+
+    const goToPlay = () => location.href = "/game";
+    const goToLogin = () => location.href = isLoggedIn ? "/logout" : "/login";
+
+    return <>
+        <Head>
+            <title>Random Square</title>
+        </Head>
+        <div style={{ ...wrapperStyle, height: "100vh" }}>
+            {/*@ts-ignore*/}
+            <Header size='huge' style={headerStyle} color="black">Random Square</Header>
+            <div style={wrapperStyle}>
+                <Button size="huge" style={buttonStyle} onClick={goToPlay} color="red">Play</Button>
+                <Button size="huge" style={buttonStyle} onClick={goToLogin} color="black">{"Log " + (isLoggedIn ? "out" : "in")}</Button>
+            </div>
+        </div>
+    </>;
 }
