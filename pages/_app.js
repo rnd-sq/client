daa4ba025a12af53b1c019a3d5756911a18adee0
Revenity <87530663+aquapi@users.noreply.github.com>
2022-05-24 22:35:46 +0700

    Got 100 points in SEO Lighthouse

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
 // @ts-check
+import Head from "next/head";
 import "./styles/index.scss";
 
 export default function App({ Component, props }) {
-    return <Component {...props} />;
+    return <>
+        <Head>
+            <meta charSet="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta name="description" content="A simple paper game which uses a dice" />
+        </Head>
+        <Component {...props} />
+    </>;
 }
