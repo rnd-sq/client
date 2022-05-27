b6440e888cc3e8ef0b7fc5c70da405dab93d9fcb
Revenity <87530663+aquapi@users.noreply.github.com>
2022-05-27 16:14:09 +0700

    Update config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,15 @@
+const apis = ["/api/login", "/api/signup", "/api/maps/publish"];
+
 /**
  * @type {import("next").NextConfig}
  */
 module.exports = {
     compress: true,
     async rewrites() {
-        return [
-            {
-                source: "/api/login",
-                destination: "https://rnd-sq-dev.herokuapp.com/api/login"
-            },
-            {
-                source: "/api/signup",
-                destination: "https://rnd-sq-dev.herokuapp.com/api/signup"
-            }
-        ];
+        return apis.map(des => ({
+            source: des,
+            destination: "https://rnd-sq-dev.herokuapp.com" + des
+        }));
     },
     httpAgentOptions: {
         keepAlive: false,
