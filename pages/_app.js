17f10afb91e7bba484904bbd1cf5ea8a65fd38a1
Revenity <87530663+aquapi@users.noreply.github.com>
2022-05-28 16:49:44 +0700

    New favicon

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,7 @@ export default function App({ Component, props }) {
             <meta charSet="UTF-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <meta name="description" content="A simple paper game which uses a dice" />
+            <link rel="shortcut icon" href="/square.png" />
         </Head>
         <Component {...props} />
     </>;
