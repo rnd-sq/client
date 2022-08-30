97783a91b98cc70df95bd3549c1f728e671c1709
Revenity <87530663+aquapi@users.noreply.github.com>
2022-08-30 22:34:10 +0700

    Update server destination

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = {
     async rewrites() {
         return apis.map(des => ({
             source: des,
-            destination: (process.env.NODE_ENV === "production" ? "https://rnd-sq-dev.herokuapp.com" : "http://localhost:5000") + des
+            destination: (process.env.NODE_ENV === "production" ? "https://rnd-sq.loca.lt" : "http://localhost:5000") + des
         }));
     },
     httpAgentOptions: {
